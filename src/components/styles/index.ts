9fe7d71398137de9cb9361c654098feabb82c939
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-11-07 03:24:42 -0300

    feat(components): emotion styles

---
+++ src/components/styles/index.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+  overflow: auto;
+  padding: 32px 40px;
+`;
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+  padding: 0 4px;
+  width: 100%;
+  margin-bottom: 24px;
+`;
