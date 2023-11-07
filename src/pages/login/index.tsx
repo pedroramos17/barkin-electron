88be965a1d9556666004e20a8cc1093172be979c
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-11-07 00:52:22 -0300

    refactor(login): fix styles

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,40 +1,67 @@
 import { Link } from 'react-router-dom';
 import { TextField, Button, Typography } from '@mui/material';
+import styled from '@emotion/styled';
 import logo from '../../../assets/logo.png';
-import './style.css';
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  width: 124px;
+  margin-bottom: 24px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 28px;
+`;
 
 export default function Login() {
   return (
-    <section>
-      <img src={logo} alt="logo" />
-      <form action="/" method="post">
+    <Container>
+      <Image src={logo} alt="logo" />
+      <form
+        action="/driver"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '80%',
+          gap: 20,
+        }}
+      >
         <TextField
           type="text"
           name="email"
           id="email"
           label="Seu email"
-          required
+          // required
         />
         <TextField
           type="password"
           name="pwd"
           id="pwd"
           label="Sua senha"
-          required
+          // required
         />
-        <div className="flex justify-center">
-          <div className="w-44 space-y-8">
-            <Button type="submit" variant="contained">
-              Entrar
-            </Button>
-            <div>
-              <Typography variant="body1">
-                Não tem conta ainda? <Link to="/signup">Cadastre-se</Link>
-              </Typography>
-            </div>
+        <Wrapper>
+          <Button type="submit" variant="contained">
+            Entrar
+          </Button>
+          <div>
+            <Typography variant="body1" style={{ marginTop: 16 }}>
+              Não tem conta ainda? <Link to="/signup">Cadastre-se</Link>
+            </Typography>
           </div>
-        </div>
+        </Wrapper>
       </form>
-    </section>
+    </Container>
   );
 }
