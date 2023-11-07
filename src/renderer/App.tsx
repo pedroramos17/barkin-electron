64e51388bff856c92fcfe22ffea1a4b7af9847cb
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-11-07 00:16:16 -0300

    feat: routes

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,13 +1,29 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import EnhancedTable from '../pages/test';
+import Login from '../pages/login';
+import Signup from '../pages/signup';
+import DriverForm from '../pages/forms/driver';
+import Driver from '../pages/driver';
+import Gateway from '../pages/gateway';
 import GatewayForm from '../pages/forms/gateway';
+import Profile from '../pages/profile';
+import ProfileForm from '../pages/forms/profile';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<GatewayForm />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/driver" element={<Driver />}>
+          <Route path=":id" element={<DriverForm />} />
+        </Route>
+        <Route path="/gateway" element={<Gateway />}>
+          <Route path=":id" element={<GatewayForm />} />
+        </Route>
+        <Route path="/profile" element={<Profile />}>
+          <Route path=":id" element={<ProfileForm />} />
+        </Route>
       </Routes>
     </Router>
   );
