505d1178205243eb8c0249e6e82bcf1b772af469
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-11-07 01:23:26 -0300

    refactor(driver): fix styles

--- src/pages/forms/driver/index.tsx
+++ src/pages/forms/driver/index.tsx
@@ -1,25 +1,48 @@
-import { TextField, Button } from '@mui/material';
+import { TextField, Button, Box } from '@mui/material';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: stretch;
+  width: 100%;
+  height: 100%;
+  padding: 24px auto;
+  gap: 12px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 export default function DriverForm() {
   return (
-    <div className="h-screen w-screen py-8 px-10 flex flex-col justify-between items-stretch">
-      <h1 className="text-2xl font-bold">Cadastrar Motorista</h1>
-      <TextField label="Nome" variant="standard" />
-      <TextField label="RG" variant="standard" />
-      <TextField label="Telefone" variant="standard" />
-      <h2 className="text-xl font-semibold">Adicionar Veículo</h2>
-      <TextField label="Modelo" variant="standard" />
-      <TextField label="Cor" variant="standard" />
-      <TextField label="Placa" variant="standard" />
-      <div>
-        <Button variant="contained" color="info">
-          +1 Veículo
-        </Button>
-      </div>
-      <div className="flex justify-between">
-        <Button variant="outlined">Voltar</Button>
-        <Button variant="contained">Salvar</Button>
-      </div>
-    </div>
+    <Box sx={{ mx: 4 }}>
+      <h1>Cadastrar Motorista</h1>
+      <form action="/driver" method="post">
+        <Container>
+          <TextField label="Nome" variant="standard" />
+          <TextField label="RG" variant="standard" />
+          <TextField label="Telefone" variant="standard" />
+          <h2>Adicionar Veículo</h2>
+          <TextField label="Modelo" variant="standard" />
+          <TextField label="Cor" variant="standard" />
+          <TextField label="Placa" variant="standard" />
+          <Box sx={{ my: 4 }}>
+            <Button variant="contained" color="info">
+              +1 Veículo
+            </Button>
+          </Box>
+          <ButtonContainer>
+            <Button variant="outlined" LinkComponent="a" href="/driver">
+              Voltar
+            </Button>
+            <Button variant="contained">Salvar</Button>
+          </ButtonContainer>
+        </Container>
+      </form>
+    </Box>
   );
 }
