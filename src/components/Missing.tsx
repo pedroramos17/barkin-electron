506a6418d0385ebeafc49a16bbf4df3fedeceab6
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-11-29 23:39:08 -0300

    wip auth context

---
+++ src/components/Missing.tsx
@@ -0,0 +1,7 @@
+export default function Missing() {
+  return (
+    <div>
+      <h1>Ops! Está página não existe.</h1>
+    </div>
+  );
+}
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,11 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import AuthProvider from '../context/AuthProvider';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
-root.render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
-);
+root.render(<App />);
